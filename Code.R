efd00e6fcf72715baa6bb4e7cf88252fad627449
Wan <p20012339@student.newinti.edu.my>
2022-11-03 16:55:40 +0800

    Read all csv file

--- Code.R
+++ Code.R
@@ -1,9 +1,44 @@
-install.packages("parallel")
+#install.packages("parallel")
 
-library(parallel)
-library(MASS)
+#library(parallel)
+#library(MASS)
 
-starts <- rep(100, 40)
-fx <- function(nstart) kmeans(Boston, 4, nstart=nstart)
-numCores <- detectCores()
-numCores
+#data <- read.csv("C:/Users/lauwa/Downloads/DATASET/DATASET/Apr_borough_grocery.csv")
+
+
+#starts <- rep(100, 40)
+#fx <- function(nstart) kmeans(Boston, 4, nstart=nstart)
+#numCores <- detectCores()
+#numCores
+
+#system.time(results <- lapply(starts, fx))
+
+
+
+
+
+
+
+
+# Example 2 - Using tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+df <-
+  list.files(path = "C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET/", pattern = "*.csv") %>%
+  map_df(~read_csv(.))
+df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
