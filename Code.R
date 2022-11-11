61fd427e15aba861fdcbb11b31bdee40b41a2767
Wan <p20012339@student.newinti.edu.my>
2022-11-12 01:55:01 +0800

    Done parallel processing, update sequential processing

--- Code.R
+++ Code.R
@@ -1,40 +1,36 @@
-# Comparison between Parallel and Sequential Processing 
+# Comparison between Parallel and Sequential Processing
+
+# Set working directory
+setwd("C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET")
+
+# Using parallel package
+install.packages("parallel")
+library(parallel)
+detectCores()
 
 # Using tidyverse to read data set
 install.packages("tidyverse")
 library(tidyverse)
 
-df <-
-  list.files(path = "C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET/", pattern = "*.csv") %>%
-  map_df(~read_csv(.))
+path <- "C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET/"
+
+df <- list.files(path, pattern = "*.csv") %>%
+map_df(~read_csv(.))
 df
+
+# Using sequential
+path <- "C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET/"
+
+filenames <- list.files(path, pattern = "*.csv")
+All <- lapply(filenames,function(i){
+  read.csv(i, header=TRUE, skip=4)
+})
+
+system.time(save1 <- lapply(1:100, All))
 
 # Using microbenchmark to compare the two processing
 install.packages("microbenchmark")
 library(microbenchmark)
-
-# Using parallel package
-install.packages("parallel")
-library(parallel)
-
-f <- function(i) {
-  lmer(Petal.Width ~ . - Species + (1 | Species), data = iris)}
-
-system.time({
-  library(lme4)
-  save1 <- lapply(1:100, f)})
-
-
-# Using sequential
-
-
-
-# Start comparing
-numCores <- detectCores()
-
-results <- mclapply(1:100,
-                    FUN=function(i) read.csv(paste0("./data/datafile-", i, ".csv")),
-                    mc.cores = numCores)
 
 f <- function() NULL
 res <- microbenchmark(NULL, f(), times=1000L)
@@ -49,6 +45,7 @@ mbm <- microbenchmark("lm" = { b <- lm(y ~ X + 0)$coef },
                         b <- solve(t(X) %*% X, t(X) %*% y)
                         },
                       check = check_for_equal_coefs)
+
 mbm
 library(ggplot2)
 autoplot(mbm)
