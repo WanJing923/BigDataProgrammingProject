0f080a74b9644e77b8214808ddb2e887b60008f7
Wan <p20012339@student.newinti.edu.my>
2022-11-16 01:12:49 +0800

    Combining dataset done, data cleasing and Descriptive Analysis completed but not sure yet

--- Code.R
+++ Code.R
@@ -59,8 +59,135 @@ p
 
 # Descriptive Analysis
 
+install.packages("dplyr")
+
+library(dplyr)
+library(readr)
+
+# Import and merge three targeted CSV files
+file1 <- "C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET/Dec_borough_grocery.csv"
+file2 <- "C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET/Dec_msoa_grocery.csv"
+file3 <- "C:/Users/lauwa/Documents/BigDataProgrammingProject/BigDataProgrammingProject/DATASET/Dec_osward_grocery.csv"
+
+files <- list(file1, file2, file3)
+
+df <- files %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
+
+# View resulting data frame
+df %>% data.frame 
+
+# View all rows or columns
+df %>% print(n=Inf)
+df %>% print(width=Inf)
+
+# Keep the targeted columns: product calories, average age and number of transitions
+data = df[c("energy_density","avg_age","num_transactions")]
+data %>% print(n=Inf)
+
+str(data)
+
+# minimum, maximum and range of product calories
+min(data$energy_density)
+max(data$energy_density)
+range(data$energy_density)
+
+# minimum, maximum and range of average age
+min(data$avg_age)
+max(data$avg_age)
+range(data$avg_age)
 
 
+# minimum, maximum and range of number of transitions
+min(data$num_transactions)
+max(data$num_transactions)
+range(data$num_transactions)
+
+# mean of product calories, average age and number of transitions
+mean(data$energy_density)
+mean(data$avg_age)
+mean(data$num_transactions)
+
+# median of product calories, average age and number of transitions
+median(data$energy_density)
+median(data$avg_age)
+median(data$num_transactions)
+
+# first and third quartile of product calories, average age and number of transitions
+quantile(data$energy_density, 0.25)
+quantile(data$energy_density, 0.75)
+
+quantile(data$avg_age, 0.25)
+quantile(data$avg_age, 0.75)
+
+quantile(data$num_transactions, 0.25)
+quantile(data$num_transactions, 0.75)
+
+# interquartile range of product calories, average age and number of transitions
+IQR(data$energy_density)
+IQR(data$avg_age)
+IQR(data$num_transactions)
+
+# standard deviation and variance of product calories, average age and number of transitions
+sd(data$energy_density)
+sd(data$avg_age)
+sd(data$num_transactions)
+
+var(data$energy_density)
+var(data$avg_age)
+var(data$num_transactions)
+
+# standard deviation and variance of three columns
+lapply(data[, 1:3], sd)
+lapply(data[, 1:3], var)
+
+summary(data)
+
+# More descriptive analysis
+install.packages("pastecs")
+library(pastecs)
+stat.desc(data)
+
+# coefficient of variation
+sd(data$energy_density) / mean(data$energy_density)
+sd(data$avg_age) / mean(data$avg_age)
+sd(data$num_transactions) / mean(data$num_transactions)
+
+# Data Analysis
+
+# Correlation Test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Hypothesis Testing
 
 
 
